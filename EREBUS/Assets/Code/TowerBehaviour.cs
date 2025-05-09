38fb92f3abd6f9660939b02ed4844744c493779e
yaboimarf <mfolst2006@gmail.com>
2025-05-09 11:39:48 +0200

    Tower Targeting AI 0.1
    het begin van het targeting systeem voor de towers

--- EREBUS/Assets/Code/TowerBehaviour.cs
+++ EREBUS/Assets/Code/TowerBehaviour.cs
@@ -7,27 +7,48 @@ public class TowerBehaviour : MonoBehaviour
 {
     public Transform target;
     public Transform gun;
-    public float rotateSpeed;
+    public float targetRadius;
     // Use this for initialization
     void Start()
     {
-        // if no target specified, assume the player
-        if (target == null)
-        {
-
-            if (GameObject.FindWithTag("Player") != null)
-            {
-                target = GameObject.FindWithTag("Player").GetComponent<Transform>();
-            }
-        }
+        TargetingAI();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        // face the target
-        transform.LookAt(new Vector3 (target.position.x,transform.position.y, target.position.z));
-        gun.LookAt(new Vector3(target.position.x,target.position.y, gun.position.z));
+        TurretRotation();
+        TargetingAI();
+    }
+    private void TurretRotation()
+    {
+        Vector3 dir = target.position - transform.position;
+        dir.y = 0;
+        transform.rotation = Quaternion.LookRotation(dir);
+
+        Vector3 gundir = target.position - gun.position;
+        gun.rotation = Quaternion.LookRotation(gundir);
+    }
+    private void TargetingAI()
+    {
+        //Vector3 startPos = transform.position;
+        //Collider[] colliders = Physics.OverlapSphere(startPos, targetRadius);
+        //if (colliders != null)
+        //{
+        //    target = GameObject.FindWithTag("Enemy").GetComponent<Transform>();
+        //}
+        if (target == null)
+        {
+            //if (target.gameObject != null)
+            //{
+            //    target = GameObject.FindWithTag("Enemy").GetComponent<Transform>();
+            //}
+            target = GameObject.FindWithTag("Enemy").GetComponent<Transform>();
+
+            //if (GameObject.FindWithTag("Player") != null)
+            //{
+            //    target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            //}
+        }
     }
 }
