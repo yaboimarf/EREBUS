70e2021c75116da2dfddeee1cfce55fa717fecd8
yaboimarf <mfolst2006@gmail.com>
2025-05-20 20:48:24 +0200

    Tower Placement 1.0
    Tower placement werkt nu door op een knop te klikken en dan te klikken op een voor geselecteerde locatie om te plaatsen

--- EREBUS/Assets/Code/TowerPlacement.cs
+++ EREBUS/Assets/Code/TowerPlacement.cs
@@ -4,53 +4,87 @@ using UnityEngine;
 
 public class TowerPlacement : MonoBehaviour
 {
-    public GameObject tower;
-    public GameObject focusObs;
+    private GameObject focusObs;
+    [SerializeField] private Camera playerCamera;
+
     
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (focusObs != null)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(!Physics.Raycast(ray, out hit))
+            Ray camray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(camray, out RaycastHit hitInfo, 100000f))
             {
-                return;
+                focusObs.transform.position = hitInfo.point;
+                focusObs.GetComponent<SphereCollider>().enabled = false;
             }
-            focusObs = Instantiate(tower, hit.point, tower.transform.rotation);
-            focusObs.GetComponent<SphereCollider>().enabled = false;
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (!Physics.Raycast(ray, out hit))
+                {
+                    return;
+                }
+                if (hit.collider.gameObject.CompareTag("Platform") && hit.normal.Equals(new Vector3(0, 1, 0)))
+                {
+                    hit.collider.gameObject.tag = "Occupied";
+                    focusObs.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, focusObs.transform.position.y, hit.collider.gameObject.transform.position.z);
+                    focusObs.gameObject.GetComponent<SphereCollider>().enabled = true;
+                }
+                else
+                {
+                    Destroy(focusObs);
+                }                
+                focusObs = null;
+            }
         }
-        else if (Input.GetMouseButton(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (!Physics.Raycast(ray, out hit))
-            {
-                return;
-            }
-            focusObs.transform.position = hit.point;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (!Physics.Raycast(ray, out hit))
-            {
-                return;
-            }
-            if (hit.collider.gameObject.CompareTag("Platform") && hit.normal.Equals(new Vector3(0,1,0)))
-            {
-                hit.collider.gameObject.tag = "Occupied";
-                focusObs.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, focusObs.transform.position.y, hit.collider.gameObject.transform.position.z);
-                focusObs.gameObject.GetComponent<SphereCollider>().enabled = true;
-            }
-            else
-            {
-                Destroy(focusObs);
-            }
-            focusObs = null;
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if(!Physics.Raycast(ray, out hit))
+        //    {
+        //        return;
+        //    }
+        //    focusObs = Instantiate(tower, hit.point, tower.transform.rotation);
+        //    focusObs.GetComponent<SphereCollider>().enabled = false;
+        //}
+        //else if (Input.GetMouseButton(0))
+        //{
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (!Physics.Raycast(ray, out hit))
+        //    {
+        //        return;
+        //    }
+        //    focusObs.transform.position = hit.point;
+        //}
+        //else if (Input.GetMouseButtonUp(0))
+        //{
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (!Physics.Raycast(ray, out hit))
+        //    {
+        //        return;
+        //    }
+        //    if (hit.collider.gameObject.CompareTag("Platform") && hit.normal.Equals(new Vector3(0,1,0)))
+        //    {
+        //        hit.collider.gameObject.tag = "Occupied";
+        //        focusObs.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, focusObs.transform.position.y, hit.collider.gameObject.transform.position.z);
+        //        focusObs.gameObject.GetComponent<SphereCollider>().enabled = true;
+        //    }
+        //    else
+        //    {
+        //        Destroy(focusObs);
+        //    }
+        //    focusObs = null;
+        //}
+    }
+    public void SetTowerToPlace(GameObject tower)
+    {
+        focusObs = Instantiate(tower, Vector3.zero, Quaternion.identity);
     }
 }
